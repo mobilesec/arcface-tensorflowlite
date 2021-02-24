64440224bece7ec73d4a26d5bb50d5ea4a5e8aea
Philipp Hofer <philipp.hofer@protonmail.com>
2021-02-24 16:20:39 +0100

    increase version number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='arcface',
-    version='0.0.2',
+    version='0.0.3',
     author="Philipp Hofer",
     author_email="philipp.hofer@ins.jku.at",
     description="ArcFace face recognition implementation in Tensorflow Light.",
